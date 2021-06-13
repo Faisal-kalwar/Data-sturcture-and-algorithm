cf2d8c50ddd24d273a55adebec5098efaff1763d
Faisal kalwar <faisalkalwar48@gmail.com>
2021-06-13 14:18:17 +0500

    conditionals

--- Conditionals/switch statements/app3.js
+++ Conditionals/switch statements/app3.js
@@ -1,10 +1,10 @@
-
-switch(number){
-    case '< 10':
-        console.log("less than 10")
+let count;
+switch(true){
+    case (count < 10):
+        console.log("less than 10");
         break;
-    case '> 10':
-        console.log("greater than 10")
+    case (count > 10):
+        console.log("greater than 10");
         break;
     default:
         console.log("equal to 10 ");
