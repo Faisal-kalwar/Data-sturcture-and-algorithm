25ffbb7f8ef66db8af2d4fec42d0f6b0e0d479e2
Faisal kalwar <faisalkalwar48@gmail.com>
2021-06-13 10:34:12 +0500

    Conditionals Statements complete

---
+++ Conditionals/app.js
@@ -0,0 +1,19 @@
+let number = 1;
+if (number == 1){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+// 
+
+function addition(num1, num2){
+    if (num1 +num2 == 11){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+addition(4 ,7);
---
+++ Conditionals/app1.js
@@ -0,0 +1,9 @@
+function addition(num1, num2){
+    if (num1 +num2 == 11){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+addition(4 ,7);
